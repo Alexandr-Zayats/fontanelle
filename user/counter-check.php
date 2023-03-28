462b8e48a42ba4d68e8224a315a8491c44dfe2f1
Alexandr Zayats <alexandr@zayats.org>
2023-03-28 19:20:28 +0000

    - add new features

--- user/counter-check.php
+++ user/counter-check.php
@@ -13,7 +13,10 @@ if(isset($_GET['cid'])) { $cid=$_GET['cid']; }
 if (strlen($_SESSION['adid']==0) || $_SESSION['type']!="cashier") {
   header('location:logout.php');
 } else {
-  if(isset($_POST['addvalues'])) {
+  $latest=mysqli_fetch_assoc(mysqli_query($con,"call sp_getLastCounterValues($cid)"));
+
+  if(isset($_POST['submit'])) {
+  //if(isset($_POST['addvalues'])) {
     $cid=$_POST['cid'];
     $name=$_POST['сname'];
     $counterNum=$_POST['counterNum'];
@@ -22,11 +25,61 @@ if (strlen($_SESSION['adid']==0) || $_SESSION['type']!="cashier") {
     $dCurrent=$_POST['dCurrent'];
     $nCurrent=$_POST['nCurrent'];
     $location=$_POST['location'];
-  }
-  $latest=mysqli_fetch_assoc(mysqli_query($con,"call sp_getLastCounterValues($cid)"));
-  if(isset($_POST['addvalues'])) {
+
     $dayLast=$latest['dayLast'];
     $nightLast=$latest['nightLast'];
+
+    // ------ FILES upload
+    function reArrayFiles( $arr ){
+      foreach( $arr as $key => $all ){
+        foreach( $all as $i => $val ){
+          $new[$i][$key] = $val;    
+        }    
+      }
+      return $new;
+    }
+    
+    $target_dir = "uploads/" . $uid . "/";
+    if (!file_exists($target_dir)) {
+      mkdir($target_dir, 0777, true);
+    }
+
+    if ($_FILES['fileToUpload']) {
+      $file_ary = reArrayFiles($_FILES['fileToUpload']);
+      foreach ($file_ary as $file) {
+        $target_file = $target_dir . date('Ymd') . '-' . basename($file['name']);
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        $check = getimagesize($file["tmp_name"]);
+
+        if($check !== false) {
+          // Check if file already exists
+          if (!file_exists($target_file)) {
+            // Check file size
+            if ($file['size'] < 500000) {
+               // Allow certain file formats
+              if (in_array($imageFileType, array('jpg', 'png', 'jpeg', 'gif'))) {
+                if (move_uploaded_file($file['tmp_name'], $target_file)) {
+                  $msg = "Файл ". htmlspecialchars( basename( $file['name'])). " сохранен.";
+                  echo "<script>alert('" . $msg . "');</script>";
+                } else {
+                  echo "<script>alert('Произошла ошибка при сохранении файла.');</script>";
+                }
+              } else {
+                echo "<script>alert('Допустимы только JPG, JPEG, PNG, GIF файлы.');</script>";
+              }
+            } else {
+              echo "<script>alert('Файл " . $file['name'] . " слишком большой.');</script>";
+            }
+          } else {
+            echo "<script>alert('Файл " . $file['name'] . " уже существует.');</script>";
+          }
+        } else {
+          echo "<script>alert('Файл " . $file['name'] . " не является изображением.');</script>";
+        }
+      }
+    }
+    // ------- END of FILES upload
+
     if($nightLast > $nCurrent || $dayLast > $dCurrent) {
       //echo "<script>alert('$nightLast; $nCurrent; $dayLast; $dCurrent');</script>";
       echo "<script>alert('Введеные показания ниже предыдущих!');</script>";
@@ -83,8 +136,7 @@ if (strlen($_SESSION['adid']==0) || $_SESSION['type']!="cashier") {
   $uid=$_GET['uid'];
   $counter=mysqli_fetch_assoc(mysqli_query($con, "SELECT name, number, info, verDate, location FROM counters WHERE id=$cid;"));
 ?>
-<body class="bg-gradient-primary">
-
+  <body class="bg-gradient-primary">
     <div class="container">
         <div class="card o-hidden border-0 shadow-lg my-5">
             <div class="card-body p-0">
@@ -97,7 +149,7 @@ if (strlen($_SESSION['adid']==0) || $_SESSION['type']!="cashier") {
                                 <h5 style="color:blue">СТ "РУЧЕЕК"</h5>
                                 <h1 class="h4 text-gray-900 mb-4">Текущие показаний</h1>
                             </div>
-                            <form class="user" name="addvalues" method="post">
+                            <form class="user" name="submit" method="post" enctype="multipart/form-data">
                                 <div class="form-group row">
                                   <div class="col-sm-6 mb-3 mb-sm-0">
                                     <label for="uid">Участок:</label>
@@ -196,10 +248,15 @@ if (strlen($_SESSION['adid']==0) || $_SESSION['type']!="cashier") {
                                 <?php } else {?>
                                     <input type="hidden" id="nCurrent" name="nCurrent" value=0>
                                 <?php } ?>
-                                <button type="submit" name="addvalues" class="btn btn-primary btn-user btn-block">
-                                    Проверен
+                                <div class="form-group row">
+                                  <div class="col-sm-6 mb-3 mb-sm-0">
+                                    <label>Фото счетчика</label>
+                                    <input type="file" name="fileToUpload[]" multiple="multiple">
+                                  </div>
+                                </div>
+                                <button type="submit" name="submit" class="btn btn-primary btn-user btn-block">
+                                  Проверен
                                 </button>
-                           
                             </form>
                             <hr>
                         </div>
@@ -220,6 +277,5 @@ if (strlen($_SESSION['adid']==0) || $_SESSION['type']!="cashier") {
     <!-- Custom scripts for all pages-->
     <script src="../js/sb-admin-2.min.js"></script>
 
-</body>
-
+  </body>
 </html>
